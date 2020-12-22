787d2885f3859b38fa2d9503ff48d5573e42d6fe
LeKAKiD <lekakid@kakao.com>
2020-12-22 22:09:06 +0900

    Make App render InputSample

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
   return (
-    <Counter />
+    <InputSample />
   );
 }
 
